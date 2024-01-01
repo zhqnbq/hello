21a742411fb447e4bcdbf1e0344a6f1ac2232a39
赵颢淇 <101001142+zhqnbq@users.noreply.github.com>
2024-01-01 21:02:21 +0800

    Add files via upload

---
+++ thupc2017.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+#define int long long
+#define %d %lld
+using namespace std;
+struct node{
+	int p;
+	bool gender;//0m1f
+	int s;//0->50
+	int longScore;//second
+	bool passedTest;
+	int f;
+	int c;
+	int lastValidTimeStamp;
+	int score;
+	int validSunCnt;
+}data[10001];
+map<int,int> IDtoStructID;
+int p;
+char gen;
+int s;
+int min,sec;
+char testP;
+int f,c;
+int main(){
+	int n;
+	cin>>n;
+	for(int i=1;i<=n;i++){
+		cin>>p>>gen>>s;
+		
+		
+	}
+} 
